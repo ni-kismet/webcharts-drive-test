604dc22a5601f51936c992dbc558f74f633bcd2e
VirgilChindris <Virgil.Chindris@ni.com>
2017-10-10 10:28:24 +0300

    The first part of the chain looks ok.

--- performance/graphing_cog.js
+++ performance/graphing_cog.js
@@ -2,6 +2,11 @@
 
 (function () {
     'use strict';
+    const cBigCog3X = -2.5;
+    const cBigCog3Y = 0;
+    const cSmallCog3X = 3.5;
+    const cSmallCog3Y = 0;
+
     var graph;
     var graph2;
     var graph3;
@@ -32,6 +37,8 @@
     var spiral2_SampleBuffer = {x:[], y:[]};
     var spiral3_SampleBuffer = {x:[], y:[]};
 
+    var chainSampleBuffer = {x:[], y:[], loc:[]}
+
     //Properties for cog with spirals
     var cog1_MovementProperties = {
         XOffset: 0.5,
@@ -39,6 +46,19 @@
         XDir: true,
         YDir: true
     }
+
+    //boundaries of cogs with chain
+    const cBigCog3_Radius = 1;
+    const cSmallCog3_Radius = 0.333333;
+    var cBigCog3_Left = cBigCog3X - cBigCog3_Radius;
+    var cBigCog3_Right = cBigCog3X + cBigCog3_Radius;
+    var cBigCog3Y_Top = cBigCog3Y + cBigCog3_Radius;
+    var cBigCog3Y_Bottom = cBigCog3Y - cBigCog3_Radius;
+
+    var cSmallCog3_Left = cSmallCog3X - cSmallCog3_Radius;
+    var cSmallCog3_Right = cSmallCog3X + cSmallCog3_Radius;
+    var cSmallCog3Y_Top = cSmallCog3Y + cSmallCog3_Radius;
+    var cSmallCog3Y_Bottom = cSmallCog3Y - cSmallCog3_Radius;
 
     var chartStep = 9/samples;
     var initbuffer = [[]];
@@ -53,8 +73,42 @@
     }
 
 
+    //this function assumes that both cogs have the same y coordinate and the big cog is placed to the left of the small cog
+    function chainXYToLocation(xPin, yPin) {
+       if (xPin <= cBigCog3X) {
+           return 1;
+       }
+
+       if ((xPin >= cBigCog3X) && (xPin <= cSmallCog3X) && (yPin >= cSmallCog3Y)) {
+           return 2;
+       }
+
+       if (xPin >= cSmallCog3X) {
+           return 3;
+       }
+
+       if ((xPin >= cBigCog3X) && (xPin <= cSmallCog3X) && (yPin <= cSmallCog3Y)) {
+           return 4;
+       }
+       return 0; //this indicates an undefined case
+    }
+
+
+    function generateChainPinPoints(pinsSamplesArray, movementOffset, loc1_Pins, loc2_Pins, loc3_Pins, loc4_Pins, bigCogXOffset, smallCogXOffset, angleScale, bigCogVelocityScale, smallCogVelocityScale, bigCogScale, smallCogScale) {
+        var angle = 0;
+        for (var i = 0; i < loc1_Pins; i++) {
+            angle = (i + movementOffset) * angleScale * bigCogVelocityScale;
+
+            pinsSamplesArray.loc[i] = 1;
+            pinsSamplesArray.x[i] = Math.sin(angle) * bigCogVelocityScale + bigCogXOffset;
+            pinsSamplesArray.y[i] = Math.cos(angle) * bigCogVelocityScale + 0;
+        }
+    }
+
+
     function generateCogPoints(interiorSamplesArray, exteriorSamplesArray, totalSamples, movementOffset, xOffset, yOffset, angleScale, velocityScale, interiorScale, exteriorScale) {
         var angle = 0;
+
         for (var i = 0; i <= totalSamples; i++) {
             angle = (i + movementOffset) * angleScale * velocityScale;
 
@@ -93,6 +147,15 @@
 
 
     function updatePlotBufferForSpirals(thePlotBufferArray, SamplesArray, totalSamples, plotIndexInBuffer) {
+        for (var i = 0; i <= totalSamples; i++) {
+            thePlotBufferArray[plotIndexInBuffer][i] = {};
+            thePlotBufferArray[plotIndexInBuffer][i].x = SamplesArray.x[i];
+            thePlotBufferArray[plotIndexInBuffer][i].y = SamplesArray.y[i];
+        }
+    }
+
+
+    function updatePlotBufferForChain(thePlotBufferArray, SamplesArray, totalSamples, plotIndexInBuffer) {
         for (var i = 0; i <= totalSamples; i++) {
             thePlotBufferArray[plotIndexInBuffer][i] = {};
             thePlotBufferArray[plotIndexInBuffer][i].x = SamplesArray.x[i];
@@ -187,6 +250,7 @@
 
     function updateBufferForThirdGraph(plotBuffer) {
         var globalIndex_div = globalIndex / 10;
+        var globalIndexChain_div = globalIndex % 20 / 20;
         var interiorScale;
         var exteriorScale;
         var angleScale = 2 * Math.PI / isamples;
@@ -197,7 +261,7 @@
         interiorScale = 0.95;
         exteriorScale = 1.05;
         velocityScale = 1;
-        generateCogPoints(bigCogInteriorSamplesBuffer, bigCogExteriorSamplesBuffer, isamples, globalIndex_div, -3, 0, angleScale, velocityScale, interiorScale, exteriorScale);
+        generateCogPoints(bigCogInteriorSamplesBuffer, bigCogExteriorSamplesBuffer, isamples, globalIndex_div, -2.5, 0, angleScale, velocityScale, interiorScale, exteriorScale);
 
         //small cog
         interiorScale = 0.95 * 0.333333 / 1.12;
@@ -205,7 +269,10 @@
         velocityScale = 3.333333;
 
         //right cog
-        generateCogPoints(smallCog1_InteriorSamplesBuffer, smallCog1_ExteriorSamplesBuffer, isamples / 3 + 2, globalIndex_div /* + 0.0333333 * 6 */, 3, 0,  angleScale, velocityScale, interiorScale, exteriorScale);
+        generateCogPoints(smallCog1_InteriorSamplesBuffer, smallCog1_ExteriorSamplesBuffer, isamples / 3 + 2, globalIndex_div /* + 0.0333333 * 6 */, 3.5, 0,  angleScale, velocityScale, interiorScale, exteriorScale);
+
+        //chain
+        generateChainPinPoints(chainSampleBuffer, globalIndexChain_div - 25, 25, 40, 8, 40, cBigCog3X, cSmallCog3X, angleScale * 2, 1, 3.333333, cBigCog3_Radius, cSmallCog3_Radius);
 
         //Update global buffer with samples from cogs and spirals buffers
         //big cog
@@ -213,6 +280,9 @@
 
         //small cogs
         updatePlotBufferForCogs(plotBuffer, smallCog1_InteriorSamplesBuffer, smallCog1_ExteriorSamplesBuffer, isamples / 3 - 3, 1);
+
+        //chain
+        updatePlotBufferForChain(plotBuffer, chainSampleBuffer, 25 + 40 + 8 + 40, 2);
     }
 
 
@@ -251,7 +321,7 @@
 
         graph.setData(plotBuffer1);
         graph2.setData(plotBuffer2);
-        graph3.setData(plotBuffer2);
+        graph3.setData(plotBuffer3);
     }
 
     updateBufferForFirstGraph(plotBuffer1, cog1_MovementProperties.XOffset, cog1_MovementProperties.YOffset);
